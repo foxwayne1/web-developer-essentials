f8589b78266734ff5a7236788d837c527a6e0367
foxwayne1 <fox.wayne1@gmail.com>
2023-03-07 10:30:56 +0000

    ejs files added

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const path = require("path");
+
+const authRoutes = require("./routes/auth.router");
+
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
+app.use(authRoutes);
+
 app.get("/", (req, res) => {
-  res.send("Working, Bitch");
+  res.send("<h1>Working, Bitch</h1>");
 });
 
-app.listen(3001, () => console.log(`listening at http://localhost:3001`));
+app.listen(3000, () => console.log(`listening at http://localhost:3000`));
---
+++ controllers/auth.controller.js
@@ -0,0 +1,11 @@
+// const express = require("express");
+
+function getSignup(req, res) {
+  res.render("customer/auth/signup");
+}
+
+function getLogin(req, res) {
+  res.render("customer/auth/login");
+}
+
+module.exports = { getSignup, getLogin };

50414b17fbd3d5a22318bbe65f038dcc84c4cafa
foxwayne1 <fox.wayne1@gmail.com>
2023-03-07 14:36:48 +0000

    sessions added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const User = require('../models/user.model')
+const authUtil = require('../util/authentication')
 
 function getSignup(req, res) {
   res.render('customer/auth/signup')
@@ -23,4 +24,27 @@ function getLogin(req, res) {
   res.render('customer/auth/login')
 }
 
-module.exports = { getSignup, getLogin, signup }
+async function login(req, res) {
+  const user = new User(req.body.email, req.body.password)
+  const existingUser = await user.getUserWithSameEmail()
+
+  if (!existingUser) {
+    res.redirect('/login')
+    return
+  }
+
+  const passwordIsCorrect = await user.hasMatchingPassword(
+    existingUser.password
+  )
+
+  if (!passwordIsCorrect) {
+    res.redirect('/login')
+    return
+  }
+
+  authUtil.createUserSession(req, existingUser, function () {
+    res.redirect('/')
+  })
+}
+
+module.exports = { getSignup, getLogin, signup, login }
